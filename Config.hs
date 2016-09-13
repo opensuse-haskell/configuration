cc9e3746cbec5478160ff5d7273b557d66414991
Peter Simons <simons@cryp.to>
2016-09-13 11:42:58 +0200

    Improve config file reading support.

--- Config.hs
+++ Config.hs
@@ -3,13 +3,24 @@
 
 module Config where
 
+import Control.Monad
+import Control.Exception
+import Data.Set as Set
+import Data.Map as Map
+import Distribution.Verbosity
+import Data.Maybe
+import Development.Shake
+import Development.Shake.Classes
+import Development.Shake.FilePath
+import Distribution.Compiler
 import Distribution.Package
 import Distribution.PackageDescription
-import Distribution.Compiler
-import Data.Map ( Map )
+import Distribution.Text
 import GHC.Generics ( Generic )
-import Development.Shake.Classes
+import System.IO.Error
 import Orphans ()
+import Distribution.Version
+import Distribution.PackageDescription.Parse
 
 newtype BuildName = BuildName { unBuildName :: String }
   deriving (Show, Eq, Ord)
@@ -26,7 +37,7 @@ data BuildDescription = BuildDescription
   , hasLib :: Bool
   , hasExe :: [FilePath]
   , forcedExe :: Bool
-  , flags :: FlagAssignment
+  , flags :: String
   }
   deriving (Show, Eq, Ord, Generic)
 
@@ -38,9 +49,89 @@ instance Package BuildDescription where
   packageId = pid
 
 data PackageSet = PackageSet
-  { pkgs :: Map BuildName BuildDescription
+  { packages :: Map BuildName BuildDescription
   , compiler :: CompilerId
   }
   deriving (Show, Eq, Ord)
 
 type Config = Map PackageSetId PackageSet
+
+readConfig :: FilePath -> Action Config
+readConfig p = do
+  psetTargets <- fmap PackageSetId <$> getDirectoryDirs p
+  psets <- mapM (readPackageSetConfig p) psetTargets
+  return $ Map.fromList (zip psetTargets psets)
+
+cabalFilePath :: PackageIdentifier -> FilePath
+cabalFilePath (PackageIdentifier (PackageName n) v) = "hackage" </> n </> display v </> n <.> "cabal"
+
+readPackageSetConfig :: FilePath -> PackageSetId -> Action PackageSet
+readPackageSetConfig p (PackageSetId psid) = do
+  bannedPackagesList <- readPackageNameList (p </> psid </> "banned-packages.txt")
+  extraPackagesList <- readConstraintList (p </> psid </> "extra-packages.txt")
+  forcedExecutableList <- readPackageNameList (p </> psid </> "forced-executable-packages.txt")
+  flagAssignment <- readFlagAssignents (p </> psid </> "flag-assignment.txt")
+  compilerIdBuf <- readFile' (p </> psid </> "compiler.txt")
+  stackagePackageList <- parseCabalConfig <$> readFile' (p </> psid </> "stackage-packages.txt")
+  cid <- parseText "compiler id" compilerIdBuf
+  let stackagePackages = Prelude.filter (\(PackageIdentifier pn _) -> pn `notElem` bannedPackagesList) stackagePackageList
+  need [ p </> ".." </>  cabalFilePath pId | pId <- stackagePackageList ]
+  pkgs <- forP stackagePackages $ \pId@(PackageIdentifier pn@(PackageName n) _) -> do
+    cabal <- liftIO $ readPackageDescription silent (p </> ".." </> cabalFilePath pId)
+    let bn = BuildName n
+        bd = BuildDescription
+                 { pid = pId
+                 , prv = Revision (maybe 0 read (Prelude.lookup "x-revision" (customFieldsPD (packageDescription cabal))))
+                 , hasLib = isJust (library (packageDescription cabal))
+                 , hasExe = Prelude.map exeName (executables (packageDescription cabal))
+                 , forcedExe = pn `elem` forcedExecutableList
+                 , flags = fromMaybe "" (Prelude.lookup n flagAssignment)
+                 }
+    return (bn,bd)
+  return $ PackageSet (Map.fromList pkgs) cid
+
+parseText :: (Text a, Monad m) => String -> String -> m a
+parseText errM buf =
+  maybe (fail ("invalid " ++ errM ++ ": " ++ show buf))
+        return
+        (simpleParse buf)
+
+readConfigFile :: FilePath -> Action [String]
+readConfigFile p = do
+  buf <- readFileLines p
+  return [ l | l@(c:_) <- buf, c /= '#' ]
+
+readConstraintList :: FilePath -> Action [Dependency]
+readConstraintList p = readConfigFile p >>= \x -> liftIO $  fileErrorContext p (mapM (parseText "constraint") x)
+
+readPackageNameList :: FilePath -> Action [PackageName]
+readPackageNameList p = readConfigFile p >>= \x -> liftIO $ fileErrorContext p (mapM (parseText "package name") x)
+
+readFlagAssignents :: FilePath -> Action [(String,String)]
+readFlagAssignents p = fmap (fmap (unwords . words . tail) . break (==':')) <$> readConfigFile p
+
+fileErrorContext :: FilePath -> IO a -> IO a
+fileErrorContext p = modifyIOError (\e -> annotateIOError e "" Nothing (Just p))
+
+-- TODO: error handling
+parseCabalConfig :: String -> [PackageIdentifier]
+parseCabalConfig buf = dependencyToId <$> catMaybes (parseCabalConfigLine <$> lines buf)
+  where
+    parseCabalConfigLine :: String -> Maybe Dependency
+    parseCabalConfigLine ('-':'-':_) = Nothing
+    parseCabalConfigLine ('c':'o':'n':'s':'t':'r':'a':'i':'n':'t':'s':':':l) = parseCabalConfigLine l
+    parseCabalConfigLine (' ':l) = parseCabalConfigLine l
+    parseCabalConfigLine l = simpleParse (if last l == ',' then init l else l)
+
+    dependencyToId :: Dependency -> PackageIdentifier
+    dependencyToId d@(Dependency n vr) = PackageIdentifier n v
+      where v   = fromMaybe err (isSpecificVersion vr)
+            err = error ("dependencyToId: unexpected argument " ++ show d)
+
+main :: IO ()
+main = do
+  let buildDir = "/tmp/_build"
+  shakeArgs shakeOptions { shakeFiles=buildDir, shakeProgress=progressSimple } $
+    action $ do
+      config <- readConfig "../../config"
+      putNormal (show config)
--- HackageOracle.hs
+++ HackageOracle.hs
@@ -10,7 +10,7 @@ import Distribution.Text
 import Distribution.Package
 import Distribution.PackageDescription
 import Distribution.Version
-import Config
+-- import Config
 
 cacheHackageDB :: Rules (() -> Action Hackage)
 cacheHackageDB = newCache $ \() -> do
@@ -18,28 +18,28 @@ cacheHackageDB = newCache $ \() -> do
   need [p]
   liftIO (readHackage' p)
 
-resolveConstraint :: Monad m => Dependency -> Hackage -> m GenericPackageDescription
-resolveConstraint c@(Dependency (PackageName name) vrange) hackage
+resolveConstraint :: Action Hackage -> Dependency -> Action GenericPackageDescription
+resolveConstraint getHackage c@(Dependency (PackageName name) vrange)
   | Just vset' <- Map.lookup name hackage
   , vset <- Set.filter (`withinRange` vrange) (Map.keysSet vset')
   , not (Set.null vset) = return (vset' ! Set.findMax vset)
   | otherwise           = fail ("cannot resolve " ++ show (display c) ++ " in Hackage")
 
-hackageOracle :: Action Hackage -> Rules (Dependency -> Action BuildDescription)
-hackageOracle getHackage = addOracle $ \x ->
-  fmap cabal2bd (getHackage >>= resolveConstraint x)
+-- hackageOracle :: Action Hackage -> Rules (Dependency -> Action BuildDescription)
+-- hackageOracle getHackage = addOracle $ \x ->
+--   fmap cabal2bd (getHackage >>= resolveConstraint x)
 
-cabal2bd :: GenericPackageDescription -> BuildDescription
-cabal2bd cabal = BuildDescription
-  { pid = packageId cabal
-  , prv = Revision (maybe 0 read (lookup "x-revision" (customFieldsPD pd)))
-  , hasLib = isJust (library pd)
-  , hasExe = map exeName (executables pd)
-  , forcedExe = False
-  , flags = []
-  }
-  where
-    pd = packageDescription cabal
+-- cabal2bd :: GenericPackageDescription -> BuildDescription
+-- cabal2bd cabal = BuildDescription
+--   { pid = packageId cabal
+--   , prv = Revision (maybe 0 read (lookup "x-revision" (customFieldsPD pd)))
+--   , hasLib = isJust (library pd)
+--   , hasExe = map exeName (executables pd)
+--   , forcedExe = False
+--   , flags = []
+--   }
+--   where
+--     pd = packageDescription cabal
 
 -- main :: IO ()
 -- main = do
