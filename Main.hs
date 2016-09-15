b212d4581389ec011ca396d2552da62edf46adbb
Peter Simons <simons@cryp.to>
2016-09-15 14:03:32 +0200

    Fix path to package id lookup.
    The naive version didn't cope with cases like "ghc-mtl", which can map
    to either "ghc-mtl" (if it's an executable package) or "mtl" (if it's a
    library).

--- Main.hs
+++ Main.hs
@@ -38,6 +38,31 @@ main = do
     compilerId <- getCompiler configDir
     flagAssignments <- getFlagAssignments configDir
 
+    getBuildName <- addOracle $ \(psid@(PackageSetId _), pkgid@(PackageIdentifier _ _)) -> do
+      fexeset <- forcedExes (GetForcedExes psid)
+      cabal <- getCabal pkgid
+      let forceExe = packageName pkgid `elem` fexeset
+          prefix | forceExe || not (hasLibrary cabal)  = ""
+                 | otherwise                           = "ghc-"
+      return $ BuildName (prefix ++ unPackageName (packageName pkgid))
+
+    pkgidFromPath <- addOracle $ \(PackageSetId psid, BuildName bn) -> do
+      pset <- askOracle (GetPackageList (PackageSetId psid)) :: Action [PackageIdentifier]
+      id1 <- case find (\(PackageIdentifier (PackageName n) _) -> n == bn) pset of
+               Nothing -> return Nothing
+               Just pkgid -> do BuildName bn' <- getBuildName (PackageSetId psid, pkgid)
+                                return (if bn == bn' then Just pkgid else Nothing)
+      id2 <- case stripPrefix "ghc-" bn of
+               Nothing -> return Nothing
+               Just bn' -> case find (\(PackageIdentifier (PackageName n) _) -> n == bn') pset of
+                             Nothing -> return Nothing
+                             Just pkgid -> do BuildName bn'' <- getBuildName (PackageSetId psid, pkgid)
+                                              return (if bn == bn'' then Just pkgid else Nothing)
+      case (id1,id2) of
+        (Nothing, Nothing) -> fail $ "no package called " ++ show bn ++ " configured in " ++ psid ++ " package set"
+        (Just pkgid, _)    -> return pkgid
+        (_, Just pkgid)    -> return pkgid
+
     -- Compute all configured builds and register the required targets.
     action $ do
       psets <- fmap PackageSetId <$> getDirectoryDirs configDir
@@ -77,26 +102,19 @@ main = do
     -- Pattern rule that copies the required source tarballs from cabal's
     -- internal cache into our build tree.
     buildDir </> "*/*/*.spec" %> \out -> do
-       let [_,psid,bn,_] = splitDirectories out
-       pset <- packageList (GetPackageList (PackageSetId psid))
-       let findPkg n = find (\(PackageIdentifier (PackageName n') _) -> n' == n) pset
-       pkgid@(PackageIdentifier pn@(PackageName n) v) <-
-         case findPkg bn of
-           Just pkgid -> return pkgid
-           Nothing -> case stripPrefix "ghc-" bn of
-                        Nothing  -> fail ("package " ++ bn ++ " is unknown")
-                        Just bn' -> case findPkg bn' of
-                                      Just pkgid -> return pkgid
-                                      Nothing -> fail ("package " ++ bn ++ " is unknown")
-       let isExe = n == bn
+       let [_,psid',bn',_] = splitDirectories out
+           psid = PackageSetId psid'
+           bn = BuildName bn'
+       pkgid@(PackageIdentifier (PackageName n) _) <- pkgidFromPath (psid,bn)
+       let isExe = n == bn'
            pkgDir = takeDirectory out
-       cid <- compilerId (GetCompiler (PackageSetId psid))
-       fas <- flagAssignments (GetFlagAssignments (PackageSetId psid))
+       cid <- compilerId (GetCompiler psid)
+       fas <- flagAssignments (GetFlagAssignments psid)
        cabal <- getCabal pkgid
        let rev = packageRevision cabal
        if rev == 0
           then liftIO $ removeFiles pkgDir ["*.cabal"]
-          else need [ show rev <.> "cabal" ]
+          else copyFileChanged (cabalFilePath hackageDir pkgid) (show rev <.> "cabal")
        -- cabal-rpm breaks if these files exist when it's run.
        liftIO $ removeFiles pkgDir ["*.spec", display pkgid]
        command_ [Cwd pkgDir, EchoStdout False]
@@ -110,7 +128,7 @@ main = do
                  ])
        command_ [] "spec-cleaner" ["-i", out]
        patches <- getDirectoryFiles "" [ "patches/common/" ++ n ++ "/*.patch"
-                                       , "patches/" ++ psid ++ "/" ++ n ++ "/*.patch"
+                                       , "patches/" ++ psid' ++ "/" ++ n ++ "/*.patch"
                                        ]
        need patches
        forM_ (sort patches) $ \p -> do
@@ -118,13 +136,9 @@ main = do
        Exit c <- command [] "grep" ["--silent", "-E", "^License:.*Unknown", out]
        when (c == ExitSuccess) $ fail "invalid license type 'Unknown'"
 
+   -- Pattern rule to copy revised Cabal files into the package directory.
+   -- buildDir </> "*/*/*.cabal" %> \out -> undefined
 
---     getCompilerVersion <- addOracle $ \(StackageVersion stackageVersion) ->
---       stripSpaces <$> readFile' ("config" </> stackageVersion </> "compiler")
---
---     getFlagAssignment <- addOracle $ \(PackageName pn) ->
---       return (flagAssignment pn)
---
 --     action $ do
 --       ls <- forM (nub (concat packageSets)) $ \p@(PackageIdentifier (PackageName n) v) -> do
 --         SusePackageDescription _ isExe <- getSusePkgDescription p
@@ -134,16 +148,6 @@ main = do
 --         return $ show n ++ "," ++ show pv ++ "," ++ show url
 --       liftIO $ writeFile (buildDir </> "package-list.csv") (intercalate "\n" ls)
 --
---     forM_ (nub (concat packageSets)) $ \p@(PackageIdentifier (PackageName pn) v) -> do
---       let pv = display v
---           pid = display p
---           tarsrc = homeDir </> ".cabal/packages/hackage.haskell.org" </> pn </> pv </> pid <.> "tar.gz"
---       tarsrc %> \out -> do
---         b <- liftIO $ System.Directory.doesFileExist out
---         unless b $
---           command_ [] "cabal" ["fetch", "-v0", "--no-dependencies", "--", display p]
---
---     -- TODO: use pattern rules instead of a loop (https://github.com/opensuse-haskell/cabal2obs/issues/2)
 --     forM_ (zip stackageVersions packageSets) $ \(stackageVersion,stackage) ->
 --       forM_ stackage $ \p@(PackageIdentifier (PackageName pn) v) -> do
 --         let forcedExe = pn `elem` forcedExecutablePackages
@@ -198,226 +202,3 @@ main = do
 --                  [ "patch --no-backup-if-mismatch --force <../../../" ++ pt | pt <- patches ] ++
 --                  [ "if grep >&2 -E '^License:.*Unknown' " ++ pkgName <.> "spec" ++ "; then exit 1; fi"
 --                  ] ++ if rv == 0 then ["rm -f ?.cabal"] else []
---
--- bash :: [String] -> Action ()
--- bash cmds = command_ [] "bash" ["-c", intercalate "; " cmds']
---   where
---     cmds' = "set -eu -o pipefail" : "shopt -s nullglob" : cmds
---
--- stripSpaces :: String -> String
--- stripSpaces = reverse . dropWhile isSpace . reverse . dropWhile isSpace
---
--- hackageRevision :: Hackage -> PackageIdentifier -> Int
--- hackageRevision hackage (PackageIdentifier (PackageName n) v) =
---   maybe 0 read (lookup "x-revision" (customFieldsPD (packageDescription (hackage ! n ! v))))
---
--- isLibrary :: Hackage -> PackageIdentifier -> Bool
--- isLibrary hackage (PackageIdentifier (PackageName n) v) =
---   isJust (condLibrary (hackage ! n ! v))
---
--- parseCabalConfig :: String -> [PackageIdentifier]
--- parseCabalConfig buf = filter cleanup $ dependencyToId <$> catMaybes (parseCabalConfigLine <$> lines buf)
---   where
---     cleanup :: PackageIdentifier -> Bool
---     cleanup (PackageIdentifier (PackageName n) _) = n `notElem` (corePackages ++ bannedPackages)
---
---     parseCabalConfigLine :: String -> Maybe Dependency
---     parseCabalConfigLine ('-':'-':_) = Nothing
---     parseCabalConfigLine ('c':'o':'n':'s':'t':'r':'a':'i':'n':'t':'s':':':l) = parseCabalConfigLine l
---     parseCabalConfigLine (' ':l) = parseCabalConfigLine l
---     parseCabalConfigLine l = simpleParse (if last l == ',' then init l else l)
---
---     dependencyToId :: Dependency -> PackageIdentifier
---     dependencyToId d@(Dependency n vr) = PackageIdentifier n v
---       where v   = fromMaybe err (isSpecificVersion vr)
---             err = error ("dependencyToId: unexpected argument " ++ show d)
---
--- -- TODO: move into config file (https://github.com/opensuse-haskell/cabal2obs/issues/3)
--- corePackages :: [String]
--- corePackages =
---   [ "array"
---   , "base"
---   , "bin-package-db"
---   , "binary"
---   , "bytestring"
---   , "Cabal"
---   , "containers"
---   , "deepseq"
---   , "directory"
---   , "filepath"
---   , "ghc"
---   , "ghc-prim"
---   , "haskeline"
---   , "hoopl"
---   , "hpc"
---   , "integer-gmp"
---   , "pretty"
---   , "process"
---   , "template-haskell"
---   , "terminfo"
---   , "time"
---   , "transformers"
---   , "unix"
---   ]
---
--- -- TODO: move into config file (https://github.com/opensuse-haskell/cabal2obs/issues/3)
--- bannedPackages :: [String]
--- bannedPackages =
---   [ "eventstore"        -- doesn't work on 32 bit: https://github.com/YoEight/eventstore/issues/51
---   , "cabal2nix"         -- needs obsolete dependencies we don't want to provide
---   , "freenect"          -- we have no working freenect
---   , "github-types"      -- no license information: https://github.com/wereHamster/github-types/issues/2
---   , "github-webhook-handler"            -- depends on broken github-types
---   , "github-webhook-handler-snap"       -- depends on broken github-types
---   , "hfsevents"         -- doesn't work on Linux
---   , "hocilib"           -- we don't have libocilib.so
---   , "hopenpgp-tools"    -- depends on broken wl-pprint-terminfo
---   , "ide-backend-rts"   -- deprecated
---   , "lhs2tex"           -- build is hard to fix
---   , "luminance-samples" -- example / tutorial code
---   , "reedsolomon"       -- needs an old version of LLVM
---   , "seqalign"          -- doesn't work on 32 bit: https://github.com/eurekagenomics/SeqAlign/issues/2
---   , "stackage"          -- no dummy packages here
---   , "stackage-build-plan" -- deprecated
---   , "stackage-cabal"    -- deprecated
--- --  , "stackage-cli"      -- deprecated (but still dependency of stackage-curator)
---   , "stackage-sandbox"  -- deprecated
---   , "stackage-setup"    -- deprecated
---   , "stackage-types"    -- deprecated
---   , "Win32"             -- doesn't work on Linux
---   , "Win32-extras"      -- doesn't work on Linux
---   , "Win32-notify"      -- doesn't work on Linux
---   , "wl-pprint-terminfo" -- https://github.com/opensuse-haskell/configuration/issues/7
---   ]
---
--- -- TODO: move into config file (https://github.com/opensuse-haskell/cabal2obs/issues/3)
--- forcedExecutablePackages :: [String]
--- forcedExecutablePackages =
---   [ "Agda"
---   , "alex"
---   , "apply-refact"
---   , "asciidiagram"
---   , "BlogLiterately"
---   , "BNFC"
---   , "bustle"
---   , "c2hs"
---   , "cab"
---   , "cabal-install"
---   , "cabal-rpm"
---   , "cabal2nix"
---   , "codex"
---   , "cpphs"
---   , "cryptol"
---   , "darcs"
---   , "derive"
---   , "diagrams-haddock"
---   , "dixi"
---   , "doctest"
---   , "doctest-discover"
---   , "find-clumpiness"
---   , "ghc-imported-from"
---   , "ghc-mod"
---   , "ghcid"
---   , "git-annex"
---   , "gtk2hs-buildtools"
---   , "hackage-mirror"
---   , "hackmanager"
---   , "handwriting"
---   , "hapistrano"
---   , "happy"
---   , "HaRe"
---   , "haskintex"
---   , "HaXml"
---   , "hdevtools"
---   , "hdocs"
---   , "highlighting-kate"
---   , "hindent"
---   , "hledger"
---   , "hledger-web"
---   , "hlint"
---   , "holy-project"
---   , "hoogle"
---   , "hpack"
---   , "hpc-coveralls"
---   , "hscolour"
---   , "hsdev"
---   , "hspec-discover"
---   , "ide-backend"
---   , "idris"
---   , "keter"
---   , "leksah-server"
---   , "lhs2tex"
---   , "markdown-unlit"
---   , "microformats2-parser"
---   , "misfortune"
---   , "modify-fasta"
---   , "msi-kb-backlit"
---   , "omnifmt"
---   , "osdkeys"
---   , "pandoc"
---   , "pointfree"
---   , "pointful"
---   , "postgresql-schema"
---   , "purescript"
---   , "shake"
---   , "ShellCheck"
---   , "stack"
---   , "stack-run-auto"
---   , "stackage-curator"
---   , "stackage-curator"
---   , "stylish-haskell"
---   , "texmath"
---   , "werewolf"
---   , "wordpass"
---   , "xmobar"
---   , "xmonad"
---   , "yi"
---   ]
---
--- -- TODO: move into config file (https://github.com/opensuse-haskell/cabal2obs/issues/3)
--- flagAssignment :: String -> String
--- flagAssignment "cheapskate" = "-fdingus"
--- flagAssignment "country-codes" = "-fgenerate"
--- flagAssignment "cryptonite" = "-f-support_aesni -f-support_rdrand -f-support_blake2_sse"
--- flagAssignment "csv-conduit" = "-fbench"
--- flagAssignment "direct-sqlite" = "-fsystemlib"
--- flagAssignment "Earley" = "-fexamples"
--- flagAssignment "GLUT" = "-fbuildexamples"
--- flagAssignment "gtk3" = "-fbuild-demos"
--- flagAssignment "highlighting-kate" = "-fexecutable -fpcre-light"
--- flagAssignment "hlibsass" = "-fexternalLibsass"
--- flagAssignment "hoauth2" = "-ftest"
--- flagAssignment "hslogger" = "-fbuildtests"
--- flagAssignment "hslua" = "-fsystem-lua"
--- flagAssignment "http2" = "-fdevel"
--- flagAssignment "ipython-kernel" = "-fexamples"
--- flagAssignment "lambdacube-gl" = "-ftestclient -fexample"
--- flagAssignment "language-lua2" = "-fexes"
--- flagAssignment "leveldb-haskell" = "-fexamples"
--- flagAssignment "nested-routes" = "-fexample"
--- flagAssignment "network-transport-zeromq" = "-finstall-benchmarks -fdistributed-process-tests"
--- flagAssignment "nix-paths" = "-fallow-relative-paths"
--- flagAssignment "pandoc-citeproc" = "-ftest_citeproc"
--- flagAssignment "rethinkdb" = "-fdev"
--- flagAssignment "servant-js" = "-fexample"
--- flagAssignment "SHA" = "-fexe"
--- flagAssignment "texmath" = "-fnetwork-uri -fexecutable"
--- flagAssignment "vector-algorithms" = "-fbench"
--- flagAssignment "wai-middleware-consul" = "-fexample"
--- flagAssignment "wai-middleware-verbs" = "-fexample"
--- flagAssignment "waitra" = "-fexamples"
--- flagAssignment "weigh" = "-fweigh-maps"
--- flagAssignment "xmobar" = "-fwith_thread -fwith_utf8 -fwith_xft -fwith_xpm -fwith_mpris -fwith_dbus -fwith_iwlib -fwith_inotify"
--- flagAssignment "yesod-auth-oauth2" = "-fexample -fnetwork-uri"
--- flagAssignment "yesod-job-queue" = "-fexample"
--- flagAssignment "yesod-static-angular" = "-fexample"
--- flagAssignment _ = ""
---
--- parseExtraConfig :: Hackage -> String -> [PackageIdentifier]
--- parseExtraConfig hackage = map f . lines
---   where
---     f :: String -> PackageIdentifier
---     f n = PackageIdentifier (PackageName n) (fst (findMax (getPkg n)))
---
---     getPkg :: String -> Map Version GenericPackageDescription
---     getPkg n = fromMaybe (error ("undefined package " ++ show n)) (DB.lookup n hackage)
