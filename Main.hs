34c58d4effb437dd14f3d1ec5f23ba892f3eee04
Peter Simons <simons@cryp.to>
2016-09-16 10:56:48 +0200

    Changelog files have no rule of their own yet.

--- Main.hs
+++ Main.hs
@@ -74,7 +74,7 @@ main = do
               isExe = isForcedExe || not (hasLibrary cabal)
               bn = (if isExe then "" else "ghc-") ++ n
               pkgDir = buildDir </> unPackageSetId psid </> bn
-          return [ pkgDir </> bn <.> "spec", pkgDir </> bn <.> "changes" ]
+          return [ pkgDir </> bn <.> "spec" ]
       need (concat (concat targets))
 
     -- Pattern target to trigger source tarball downloads with "cabal get". We
