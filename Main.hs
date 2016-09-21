3d13728c2e58c1720aa9c3e91804c5fad20e9df3
Peter Simons <simons@cryp.to>
2016-09-21 16:23:12 +0200

    Make sure edited Cabal files have Unix file endings, not DOS ones.

--- Main.hs
+++ Main.hs
@@ -119,7 +119,7 @@ main = do
        let rev = packageRevision cabal
        liftIO $ removeFiles pkgDir ["*.cabal"]
        when (rev > 0) $
-          copyFileChanged (cabalFilePath hackageDir pkgid) (pkgDir </> n <.> "cabal")
+          command_ [] "dos2unix" ["--quiet", "--keepdate", "-n", cabalFilePath hackageDir pkgid, pkgDir </> n <.> "cabal"]
        -- cabal-rpm breaks if these files exist when it's run.
        liftIO $ removeFiles pkgDir ["*.spec", display pkgid]
        command_ [Cwd pkgDir, EchoStdout False]
