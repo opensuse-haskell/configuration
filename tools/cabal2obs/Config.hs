42ec1e99eedc76469dc8a4f1f36210b7fa8d4280
Peter Simons <psimons@suse.com>
2017-07-27 09:58:43 +0200

    Enable lts-9 package set.

--- tools/cabal2obs/Config.hs
+++ tools/cabal2obs/Config.hs
@@ -1,11 +1,13 @@
 module Config where
 
 import Config.LTS8
+import Config.LTS9
 import Types
 
 knownPackageSets :: [PackageSetId]
-knownPackageSets = [PackageSetId "lts-8"]
+knownPackageSets = [PackageSetId "lts-8", PackageSetId "lts-9"]
 
 getConfig :: PackageSetId -> PackageSetConfig
 getConfig (PackageSetId "lts-8") = lts8
+getConfig (PackageSetId "lts-9") = lts9
 getConfig psid                   = error $ "getConfig: unknown package set " ++ show psid
