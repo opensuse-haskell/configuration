f284b786a7d54dd3e56139052bc61f2a58123cdf
Peter Simons <psimons@suse.com>
2017-08-14 16:57:42 +0200

    spec-cleaner: update to latest version
    This re-writes Url as URL.

--- tools/cabal2obs/Main.hs
+++ tools/cabal2obs/Main.hs
@@ -39,7 +39,7 @@ main = do
                , shakeProgress = progressDisplay 5 putStrLn
                , shakeChange = ChangeModtimeAndDigest
                , shakeThreads = 0       -- autodetect the number of available cores
-               , shakeVersion = "6"     -- version of the build rules, bump to trigger full re-build
+               , shakeVersion = "7"     -- version of the build rules, bump to trigger full re-build
                }
 
   shakeArgs shopts $ do
