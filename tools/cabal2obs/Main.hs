291a79f269ff15ebf36a7d950b229121d5277dec
Peter Simons <psimons@suse.com>
2018-03-12 11:27:14 +0100

    cabal2spec: update to latest version

--- tools/cabal2obs/Main.hs
+++ tools/cabal2obs/Main.hs
@@ -46,7 +46,7 @@ main = do
                , shakeProgress = progressDisplay 5 putStrLn
                , shakeChange = ChangeModtimeAndDigest
                , shakeThreads = 0       -- autodetect the number of available cores
-               , shakeVersion = "13"    -- version of the build rules, bump to trigger full re-build
+               , shakeVersion = "14"    -- version of the build rules, bump to trigger full re-build
                }
 
   shakeArgs shopts $ do
