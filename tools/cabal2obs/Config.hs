b0198581757b455ea8b84a4ca6a535713d3174f5
Peter Simons <psimons@suse.com>
2018-03-13 20:46:37 +0100

    nightly: track the nightly package set again to get GHC 8.4.1
    Use nightly-2018-03-13 initially.

--- tools/cabal2obs/Config.hs
+++ tools/cabal2obs/Config.hs
@@ -7,7 +7,7 @@ import Config.Nightly
 import Types
 
 knownPackageSets :: [PackageSetId]
-knownPackageSets = [PackageSetId "lts-10"]
+knownPackageSets = [PackageSetId "lts-10", PackageSetId "nightly"]
 
 getConfig :: PackageSetId -> PackageSetConfig
 getConfig (PackageSetId "lts-8")   = lts8
