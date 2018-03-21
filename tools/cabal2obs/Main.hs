f6b2221cd90080c9d5857496476f89d3c16170b6
Peter Simons <psimons@suse.com>
2018-03-21 11:40:23 +0100

    cabal2spec: update to new version with even better sub-library support

--- tools/cabal2obs/Main.hs
+++ tools/cabal2obs/Main.hs
@@ -45,7 +45,7 @@ main = do
                , shakeProgress = progressDisplay 5 putStrLn
                , shakeChange = ChangeModtimeAndDigest
                , shakeThreads = 0       -- autodetect the number of available cores
-               , shakeVersion = "17"    -- version of the build rules, bump to trigger full re-build
+               , shakeVersion = "18"    -- version of the build rules, bump to trigger full re-build
                }
 
   shakeArgs shopts $ do
