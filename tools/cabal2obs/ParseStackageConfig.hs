ff767628737fd94bcdb3962f24393a847b89c544
Peter Simons <psimons@suse.com>
2018-07-07 22:25:47 +0200

    Start modernizing the Cabal parser.

--- tools/cabal2obs/ParseStackageConfig.hs
+++ tools/cabal2obs/ParseStackageConfig.hs
@@ -29,3 +29,39 @@ runP p str = case [ r | (r,"") <- readP_to_S p str] of
                [r] -> return r
                []  -> fail "no parse"
                _   -> fail "ambiguous parse"
+
+
+-- import Control.Applicative
+-- import Data.Functor
+-- import Distribution.Compat.CharParsing
+-- import Distribution.Package
+-- import Distribution.Parsec.Class
+-- import Distribution.Parsec.FieldLineStream
+-- import Distribution.Version
+
+-- stackageConfig :: ParsecParser [Dependency]
+-- stackageConfig = many comment *> constraints
+
+-- constraints :: ParsecParser [Dependency]
+-- constraints = string "constraints:" *> sepBy (token constraint) (char ',')
+
+-- constraint :: ParsecParser Dependency
+-- constraint = try installed <|> parsec
+
+-- installed :: ParsecParser Dependency
+-- installed = Dependency <$> parsec <*> (token (string "installed") $> noVersion)
+
+-- token :: CharParsing p => p a -> p a
+-- token p = spaces *> p
+
+-- comment :: ParsecParser ()
+-- comment = skipOptional (string "--" *> (many (satisfy (/= '\n')))) <* (void (char '\n') <|> eof)
+
+-- parse :: Monad m => ParsecParser a -> String -> m a
+-- parse p x = either (fail . show) return (runParsecParser p "" (fieldLineStreamFromString x))
+
+-- input :: IO String
+-- input = readFile "/home/simons/src/opensuse-haskell/_build/cabal-lts-11.config"
+
+-- foo :: IO [Dependency]
+-- foo = input >>= parse stackageConfig
