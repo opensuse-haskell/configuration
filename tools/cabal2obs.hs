2393cc2f120e668f706462d660ed393f5985963c
Peter Simons <psimons@suse.com>
2018-09-27 15:26:42 +0200

    tools: bump cabal2obs version for a clean re-build

--- tools/cabal2obs.hs
+++ tools/cabal2obs.hs
@@ -48,7 +48,7 @@ main = do
                , shakeProgress = progressDisplay 5 putStrLn
                , shakeChange = ChangeModtimeAndDigest
                , shakeThreads = 0       -- autodetect the number of available cores
-               , shakeVersion = "29"    -- version of the build rules, bump to trigger full re-build
+               , shakeVersion = "30"    -- version of the build rules, bump to trigger full re-build
                }
 
   shakeArgs shopts $ do
