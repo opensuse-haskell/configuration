998979e51ab5a0f79839e44b5e1350fb57fe581e
Peter Simons <psimons@suse.com>
2020-08-27 19:37:21 +0200

    cabal2obs: drop unused code from ParseUtils.hs

--- src/cabal2obs/ParseUtils.hs
+++ src/cabal2obs/ParseUtils.hs
@@ -1,30 +1,9 @@
-module ParseUtils where
+module ParseUtils ( parseText ) where
 
-import Development.Shake
-import Distribution.Package
 import Distribution.Parsec
-import System.IO.Error
-import Data.Char
-
-readConfigFile :: FilePath -> Action [String]
-readConfigFile p = do
-  buf <- readFileLines p
-  return [ l | l@(c:_) <- buf, c /= '#' ]
-
-readConstraintList :: FilePath -> Action [Dependency]
-readConstraintList p = readConfigFile p >>= \x -> liftIO $ fileErrorContext p (mapM (parseText "constraint") x)
-
-readPackageNameList :: FilePath -> Action [PackageName]
-readPackageNameList p = readConfigFile p >>= \x -> liftIO $ fileErrorContext p (mapM (parseText "package name") x)
-
-fileErrorContext :: FilePath -> IO a -> IO a
-fileErrorContext p = modifyIOError (\e -> annotateIOError e "" Nothing (Just p))
 
 parseText :: (Parsec a, MonadFail m) => String -> String -> m a
 parseText errM buf =
   maybe (fail ("invalid " ++ errM ++ ": " ++ show buf))
         return
         (simpleParsec buf)
-
-stripSpaces :: String -> String
-stripSpaces = reverse . dropWhile isSpace . reverse . dropWhile isSpace
