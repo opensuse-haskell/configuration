d6fd5e1673268edaa0157869a8f0b3cb6b677089
Peter Simons <psimons@suse.com>
2022-02-08 12:29:31 +0100

    ghc-8.10.x: don't update statistics to version 0.16.x yet

--- src/cabal2obs/Config/Ghc810x.hs
+++ src/cabal2obs/Config/Ghc810x.hs
@@ -524,7 +524,7 @@ constraintList = [ "abstract-deque"
                  , "split"
                  , "splitmix"
                  , "StateVar"
-                 , "statistics"
+                 , "statistics < 0.16"  -- the latest version is not supported by criterion yet
                  , "statistics-linreg"
                  , "stm-chans"
                  , "store-core"
