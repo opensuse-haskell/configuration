9c65cc43abaeda1bb276892bfd9926e60b9d422a
Peter Simons <psimons@suse.com>
2022-02-13 10:12:31 +0100

    Require the latest distribution-opensuse version 1.1.3.

--- src/cabal2obs/cabal2obs.hs
+++ src/cabal2obs/cabal2obs.hs
@@ -40,7 +40,7 @@ main = do
                , shakeProgress = progressDisplay 5 putStrLn
                , shakeChange = ChangeModtimeAndDigest
                , shakeThreads = 0       -- autodetect the number of available cores
-               , shakeVersion = "41"    -- version of the build rules, bump to trigger full re-build
+               , shakeVersion = "42"    -- version of the build rules, bump to trigger full re-build
                }
 
   shakeArgs shopts $ do
