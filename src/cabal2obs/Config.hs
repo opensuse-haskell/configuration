b2b33c80a35923b3e89e4546dbf66533d6d0a655
Peter Simons <psimons@suse.com>
2023-02-01 12:14:27 +0100

    Create ghc-9.4.x package set and build that one by default.

--- src/cabal2obs/Config.hs
+++ src/cabal2obs/Config.hs
@@ -7,6 +7,7 @@ module Config ( knownPackageSets, addConfigOracle ) where
 import Config.Ghc810x
 import Config.Ghc90x
 import Config.Ghc92x
+import Config.Ghc94x
 import Types
 
 import Data.Map.Strict ( Map, findWithDefault, keysSet )
@@ -22,6 +23,7 @@ packageSets :: Map PackageSetId (Action PackageSetConfig)
 packageSets = [ ("ghc-8.10.x", ghc810x)
               , ("ghc-9.0.x", ghc90x)
               , ("ghc-9.2.x", ghc92x)
+              , ("ghc-9.4.x", ghc94x)
               ]
 
 knownPackageSets :: Set PackageSetId
--- src/cabal2obs/cabal2obs.hs
+++ src/cabal2obs/cabal2obs.hs
@@ -93,7 +93,7 @@ main = do
 
     -- Depend on all active package set targets.
     phony "all" $
-      need ["ghc-8.10.x", "ghc-9.2.x"]
+      need ["ghc-9.4.x"]
 
     -- Every (phony) package set target depends on the (real) spec file.
     forM_ (Set.toList knownPackageSets) $ \psid ->
