b9c91130f9254940327d0e23047e21c1cd35d10a
Peter Simons <psimons@suse.com>
2023-03-07 11:01:23 +0100

    ghc-9.4.x: ShellCheck doesn't like the latest fgl; added constraint

--- src/cabal2obs/Config/Ghc94x.hs
+++ src/cabal2obs/Config/Ghc94x.hs
@@ -179,7 +179,7 @@ constraintList = [ "adjunctions ^>= 4.4.2"
                  , "fast-logger ^>= 3.1.1"
                  , "fdo-notify ^>= 0.3.1"
                  , "feed ^>= 1.3.2.1"
-                 , "fgl ^>= 5.8.1.0"
+                 , "fgl >=5.7.0 && <5.8.1.0"    -- TODO: ShellCheck-9.0 doesn't accept 8.5.1.x even though it's a minor update.
                  , "file-embed ^>= 0.0.15.0"
                  , "filepath-bytestring ^>= 1.4.2.1.12"
                  , "filepattern ^>= 0.1.3"
