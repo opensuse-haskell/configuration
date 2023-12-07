dc3c93f3635929b82c3e185db38762b2f8577e79
Peter Simons <psimons@suse.com>
2023-12-07 14:38:24 +0100

    Set up cabal2obs to generate a ghc-8.10.x based SLE-15-SP5 project.

--- src/cabal2obs/cabal2obs.hs
+++ src/cabal2obs/cabal2obs.hs
@@ -93,7 +93,7 @@ main = do
 
     -- Depend on all active package set targets.
     phony "all" $
-      need ["ghc-9.6.x"]
+      need ["ghc-8.10.x"]
 
     -- Every (phony) package set target depends on the (real) spec file.
     forM_ (Set.toList knownPackageSets) $ \psid ->
