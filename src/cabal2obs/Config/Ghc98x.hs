8157e941849a188869ab6625eb8d0779fccdd0ef
Peter Simons <psimons@suse.com>
2024-07-18 13:05:23 +0200

    ghc-9.8.x: disable updates to happy 1.21.0, which has been deprecated

--- src/cabal2obs/Config/Ghc98x.hs
+++ src/cabal2obs/Config/Ghc98x.hs
@@ -205,7 +205,7 @@ constraintList = [ "adjunctions ^>= 4.4.2"
                  , "haddock-library ^>= 1.11.0"
                  , "hadolint"
                  , "half ^>= 0.3.1"
-                 , "happy ^>= 1.20.1.1"
+                 , "happy >= 1.20.1.1 && (< 1.21.0 || > 1.21.0)"  -- version 1.21.0 is deprecated
                  , "hashable ^>= 1.4.3.0"
                  , "hashtables ^>= 1.3.1"
                  , "haskell-lexer ^>= 1.1.1"
